3bb65829d8193d2d13d48fee0799dac71a4e051d
ELW-courses <EricaL848@gmail.com>
2025-10-26 17:14:14 -0400

    corrections

--- R/earthquake_read_clean_data.R
+++ R/earthquake_read_clean_data.R
@@ -53,16 +53,16 @@ eq_clean_data <- function(filepath = NA, df = NA){
   #
   eq_data <- eq_data %>%
     # Create Date from modified year (Yr), making note of age
-    dplyr::mutate(Yr = as.numeric(ifelse("Year" < 0, "Year" *-1, "Year")),
-                  Age = as.factor(ifelse("Year" < 0, "BC", "AD")),
-                  Date = paste("Yr", ifelse(is.na("Mo"), 01, "Mo"), ifelse(is.na("Dy"), 01, "Dy"), sep = "-"), .before = "Year") %>%
-    dplyr::mutate(Date = as.Date("Date", format = "%Y-%m-%d")) %>%
+    dplyr::mutate(Yr = as.numeric(ifelse(.data$Year < 0, .data$Year *-1, .data$Year)),
+                  Age = as.factor(ifelse(.data$Year < 0, "BC", "AD")),
+                  Date = paste(.data$Yr, ifelse(is.na(.data$Mo), 01, .data$Mo), ifelse(is.na(.data$Dy), 01, .data$Dy), sep = "-"), .before = .data$Year) %>%
+    dplyr::mutate(Date = as.Date(.data$Date, format = "%Y-%m-%d")) %>%
     # Remove Yr column, remove any odd columns due to loading
-    dplyr::select(-"Yr") %>%
-    filter(!is.na("Date")) %>%
+    dplyr::select(-.data$Yr) %>%
+    filter(!is.na(.data$Date)) %>%
     # Rename columns for clarity
-    rename("Month" = "Mo", "Day" = "Dy", "Hour" = "Hr",  "Minute" = "Mn", "Seconds" = "Sec",
-           "Tsunami" = "Tsu", "Volcano" = "Vol", "Magnitude" = "Mag") %>% # Set column types
+    rename("Month" = .data$Mo, "Day" = .data$Dy, "Hour" = .data$Hr,  "Minute" = .data$Mn, "Seconds" = .data$Sec,
+           "Tsunami" = .data$Tsu, "Volcano" = .data$Vol, "Magnitude" = .data$Mag) %>% # Set column types
     dplyr::mutate(across(any_of(c("Latitude", "Longitude")), ~as.numeric(as.character(.))))
   #
   return(eq_data)
@@ -111,7 +111,7 @@ eq_location_clean <- function(df){
     mutate(Locale = case_when(
       Country == "United States" ~ str_trim(sub(":.*", replacement = "", !!sym(matching_column))),
       TRUE ~ str_trim(str_extract(!!sym(matching_column), "(?<=:).+"))), .before = !!sym(matching_column)) %>%
-    mutate(Country = stringr::str_to_title("Country"), Locale = str_to_title("Locale"))
+    mutate(Country = stringr::str_to_title(.data$Country), Locale = str_to_title(.data$Locale))
   return(df)
 }
 #
@@ -187,11 +187,11 @@ eq_filtering <- function(df, MinDate = NULL, MaxDate = NULL, SelectedCountry = N
   ## Clean data to plot
   data_to_plot <- df %>%
     # Select date and columns to plot by
-    dplyr::select(Date, "Country", "Locale", "Magnitude", any_of(groupingBy)) %>%
+    dplyr::select(Date, .data$Country, .data$Locale, .data$Magnitude, any_of(groupingBy)) %>%
     # Filter to specified date range and country
     filter(Date >= min_date & Date <= max_date) %>%
     filter(if(!is.null(SelectedCountry)){
-      "Country" == paste(stringr::str_to_title(SelectedCountry))
+      .data$Country == paste(stringr::str_to_title(SelectedCountry))
     } else {
       TRUE
     }) %>%
